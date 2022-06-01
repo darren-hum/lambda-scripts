60979ae59dfcfa72a4edf9439458060e8009048c
Darren Hum <98694507+darren-hum@users.noreply.github.com>
2022-06-02 00:31:25 +0800

    next step to test another pyarrow wh

--- s3-clean-csv-to-parquet.py
+++ s3-clean-csv-to-parquet.py
@@ -1,3 +1,7 @@
+### To ingest csv files unzipped from vendor; clean column headers; convert to parquet
+### Files/Folders have spacing issues and encoded by aws
+### Column headers have special characters that might be problematic
+
 import json
 import os
 import io
@@ -5,6 +9,9 @@ import boto3
 import csv
 import pandas as pd
 import logging
+import pyarrow
+from urllib.parse import unquote_plus
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -12,34 +19,55 @@ logger.setLevel(logging.INFO)
 s3 = boto3.client('s3')
 
 def clean_text(text):
-
+    """ removes special chars """
     specialchars = "!@#$%^&*()[]{};:,./<>?\|`~-=_+\""
     replacechars = "                               " #corresponding characters in inCharSet to be replaced
     trans_dict = str.maketrans(specialchars, replacechars)
     text = text.translate(trans_dict)
 
     return text
+    
+def parse_url(text):
+    """ function parses key to remove encoding """
+    text = unquote_plus(text)
+
+    return text
+
+def filename_rm_space(filename):
+    """ returns file name without spaces """
+    filename = filename.split('/')[2]
+    filename_parts = filename.split('(')
+    part1 = filename_parts[0].strip().replace(" ", "_")
+    part2 = filename_parts[1].strip().replace(" ", "")
+    pq_filename = pq1 + "(" + pq2 + ".parquet"
+
+    return pq_filename
 
 def lambda_handler(event, context):
-    """ Read file from s3 on new file in folder"""
+    """ Event: Object Creation in S3 """
+    logger.info(event)
     
-    s3 = boto3.client('s3')
     if event:
+        # get all the params needed
         file_obj = event["Records"][0]
         bucket_name = file_obj['s3']['bucket']['name']
         file_name = file_obj['s3']['object']['key']
+        file_name = unquote_plus(file_name)
         
+        # for debugging
         logger.info(f"File_obj: {file_obj}")
         logger.info(f"bucket_name: {bucket_name}") 
-        logger.info(ffile_name: {file_name}")
+        logger.info(f"file_name: {file_name}")
         
-        pq_file_name = file_name.split('/')[2] + ".parquet"
- 
+        #reads the csv into df
         fileObject = s3.get_object(Bucket=bucket_name, Key=file_name)
         df = pd.read_csv(io.BytesIO(fileObject['Body'].read()))
         
         # clean column headers
         df.columns = [clean_text(x) for x in df.columns]
-        s3_path = "s3://dev-sim-input-data/smile_data/converted/" + pq_file_name
+        
+        # provide clean filename and save as parquet
+        pq_filename = filename_rm_space(file_name)
+        s3_path = "s3://dev-sim-input-data/smile_data/converted/" + pq_filename
         df.to_parquet(s3_path, compression='snappy')
 
